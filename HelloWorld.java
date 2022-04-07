a90a432e0425d83cc416a970242319d8a6ff2cfb
janasoumyajit <103078292+janasoumyajit@users.noreply.github.com>
2022-04-07 12:42:53 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+package april_06_2022;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+
+	      int num1 = 25, num2 = 789, num3 = 3;
+
+	      if( num1 >= num2 && num1 >= num3)
+	          System.out.println(num1+" is greater");
+
+	      else if (num2 >= num1 && num2 >= num3)
+	          System.out.println(num2+" is greater");
+
+	      else
+	          System.out.println(num3+" is greater");
+	  }
+	}
+
